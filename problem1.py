5d3cafee579c699f73fce0eaf5f6f55f2d511489
ronzy <ninjakai25@gmail.com>
2025-10-21 21:31:01 -0500

    finish problem 1 and 6

--- problem1.py
+++ problem1.py
@@ -1,5 +1,15 @@
 import numpy as np
 import scipy.linalg as linalg
+import sympy as sp
 
-# program that determines the dimension of a set as well as basis for it
-# to determine dimension of a set, its equal to the number of vectors in the basis set
+# compute rank to get dimensionality, then perform row reduction to get vectors for basis
+def getDimensionality(a):
+    a_matrix = sp.Matrix(a)
+
+    rref_matrix, pivot_cols = a_matrix.rref()
+
+    rank = np.linalg.matrix_rank(a)
+    print(f"Dimensionality: {rank}")
+    print(f"Basis: {rref_matrix}")
+
+    return rank
